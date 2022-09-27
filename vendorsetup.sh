97185d51466f9e0d5e310bc63a364f70cfe8887c
nobbisaapa <112622863+nobbisaapa@users.noreply.github.com>
2022-09-27 23:56:30 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,9 @@ git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 12.
 git clone https://github.com/crdroidandroid/android_hardware_qcom_media -b 12.1-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone https://github.com/crdroidandroid/android_hardware_qcom_audio -b 12.1-caf-sm8150 hardware/qcom-caf/sm8150/audio
 
+git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
+
+git clone -b courbet-12.1 --depth=1 https://github.com/PixelOS-Devices/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
 
 git clone https://github.com/GrapheneOS/platform_external_Camera -b 13 packages/apps/Camera
 echo "cloned hals"
